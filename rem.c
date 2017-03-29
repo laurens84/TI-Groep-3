4485b87fadd2f4652b95a1df1c8d11aaa72d9854
raubenravi <rubenrara@gmail.com>
2017-03-29 14:05:29 +0200

    Update rem.c

--- rem.c
+++ rem.c
@@ -1,12 +1,12 @@
 void rem(int afstand){
 	clearTimer(T1);
-	while (speed >= 0){
+	while (speed_left >= 0){
 		if (time1[T1] > 1000){
 			speed_left = speed_left - (30/ afstand) * 10
 			speed_right = speed_right - (30/ afstand) * 10
 			motor[motorB] = speed_left;
 			motor[motorC] = speed_right;
-			clearTimer(theTimer);
+			clearTimer(T1);
 		}
 	}
 }

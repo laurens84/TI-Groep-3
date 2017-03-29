71d565dae25f8ab10c9100ffcc37064596f935e3
GlennKoning <GlennKoning@users.noreply.github.com>
2017-03-29 14:39:08 +0200

    Create OldSensor.c

---
+++ OldSensor.c
@@ -0,0 +1,26 @@
+#define BWBLACK 50	//Black
+#define RGBBLACK 30	//Black
+
+bool leftSensor()
+{
+	if (SensorValue[BWsensor] < BWBLACK){
+		// zwart links
+		return true;
+	}
+	else{
+		// wit
+		return false;
+	}
+}
+
+bool rightSensor()
+{
+	if (SensorValue[RGBsensor] < RGBBLACK){
+		// zwart rechts
+		return true;
+	}
+	else{
+		// wit
+		return false;
+	}
+}

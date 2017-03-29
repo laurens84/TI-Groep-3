28c5aead498b92f4d955ce22657a79f87ad3addf
raubenravi <rubenrara@gmail.com>
2017-03-29 15:40:26 +0200

    Update lijnvolger.c

--- lijnvolger.c
+++ lijnvolger.c
@@ -1,4 +1,19 @@
-void Aandrijving(float sensor_left,float sensor_right){
-	motor[motorB] = sensor_left;
-	motor[motorC] = sensor_right;
+void Aandrijving(bool sensor_left,bool sensor_right){
+if (sensor_right == true && speed_right >= 5 ){
+		speed_right -= 1;
+		speed_left += 1;
+		}else if(sensor_left == false && speed_right <= 25){
+		speed_right += 1;
+		speed_left -= 1;
+	}
+	if (sensor_left == true && speed_left >= 5 ){
+		speed_left -= 1 ;
+		speed_right += 1
+	}
+	else if(sensor_left == false && speed_left <= 25){
+		speed_left += 1;
+		speed_right -= 1;
+	} 
+	motor[motorB] = speed_left
+	motor[motorC] = speed_right
 }

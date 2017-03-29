a9d0bb3142f420a6145564a9ffc169227969cbbd
laurens84 <laurens.vandersluis@student.hu.nl>
2017-03-29 11:30:00 +0200

    Added global variables

--- Robot.h
+++ Robot.h
@@ -2,7 +2,11 @@
 #define _ROBOT_H
 
 #pragma config(Sensor, S3,     sonar,          sensorSONAR)
+#pragma config(Sensor, S1,     BWsensor,       sensorLightActive)
+#pragma config(Sensor, S2,     RGBsensor,      sensorColorNxtRED)
 
+int speed_right = 25;
+int speed_left = 25;
 int object = 0;
 
 #endif /* _ROBOT_H */

a4ddb5aa87983513ee883249773c339d186f7619
laurens84 <laurens.vandersluis@student.hu.nl>
2017-03-30 10:48:23 +0200

    New header file

---
+++ header.c
@@ -0,0 +1,14 @@
+int speed_right = 25;
+int speed_left = 25;
+
+//variables voor bluetooth.c
+//long nLastXmitTimeStamp = nPgmTime;
+//long nDeltaTime         = 0;
+//const int kMaxSizeOfMessage = 30;
+//const int INBOX = 5;
+
+//#include "bluetooth.c"
+#include "rem.c"
+#include "collision.c"
+#include "lijnvolger.c"
+#include "sensors.c"

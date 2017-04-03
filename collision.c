c8c775b5856b26e3e27faef94857d61306f1f077
laurens84 <laurens.vandersluis@student.hu.nl>
2017-04-03 12:50:18 +0200

    Added commentary

--- collision.c
+++ collision.c
@@ -1,7 +1,30 @@
+/// \mainpage Author Information
+/// \section Information
+///
+///     Name:          collision.c
+///     Author:        Laurens van der Sluis
+///     Studentnumber: 1703647
+///     Date:          29-2-2017
+///
+/// \section doxygen How it works?
+///
+/// These functions allows you to let the robot look around and detect objects.
+
 void stop (){
 	motor[motorB] = 0;
 	motor[motorC] = 0;
 }
+
+/*! \brief look
+ *
+ * This function allows you to let the robot look around and detect objects.
+ * It can be called with the following three parameters: left, right and front.
+ * Depending of the parameter, the function will rotate the sensor and return 0 if there is not an object detected.
+ * If there is an object detected, it will return 1.
+ *
+ * NOTE! The sensor will not rotate back to it's default state after this function is called!
+ *
+ */
 
 int look(int d) {
 	if (d == front){
@@ -46,6 +69,13 @@ int look(int d) {
 	if (SensorValue[S3] < 30) return 1;
 	else return 0;
 }
+
+/*! \brief collision
+ *
+ * This function checks if there is an object within 30CM.
+ * If there is an object nearby it wil stop the robot and the music.
+ *
+ */
 
 void collision() {
 	while (SensorValue[S3] < 30) {

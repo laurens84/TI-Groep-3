bcf7a8639c8a61679514e5626c03e01d377cc8bf
raubenravi <rubenrara@gmail.com>
2017-04-03 14:51:17 +0200

    rem.c

--- rem.c
+++ rem.c
@@ -7,7 +7,6 @@ if (afstand <= 10){
 		motor[motorC] = speed_right;
 		return;
 }
-
 if (speed_left > speed_right){
 	speed_right = speed_left;
 } else {
@@ -27,8 +26,8 @@ clearTimer(T1);
 		
 		//om de duizend microseconde word de snelheid bij gewerkt.
 	if (time1[T1] >= 350){
-			speed_left = speed_left -   ((30/ afstand) * 10 ) ;//((((float) SensorValue[RGBLsensor] - RGBMIN) / (RGBMAX-RGBMIN)));
-			speed_right = speed_right - ((30/ afstand) * 10 ) ;//((((float) SensorValue[RGBRsensor] - RGBMIN) / (RGBMAX-RGBMIN)));
+			speed_left = speed_left -   ((30/ afstand) * 10 );
+			speed_right = speed_right - ((30/ afstand) * 10 );
 
 			//checkt als niet 1 van snelheidswaarde niet op min staan
 			if (speed_left < 0){
@@ -37,8 +36,8 @@ clearTimer(T1);
 			if (speed_right < 0){
 			speed_right = 0;
 			}
-			motor[motorB] = speed_left;
-			motor[motorC] = speed_right;
+			motor[motorB] = (int) (speed_left * ((((float) SensorValue[RGBLsensor] - RGBMIN) / (RGBMAX-RGBMIN))));
+			motor[motorC] = (int) (speed_right * ((((float) SensorValue[RGBRsensor] - RGBMIN) / (RGBMAX-RGBMIN))));
 			clearTimer(T1);
 		}
 		}

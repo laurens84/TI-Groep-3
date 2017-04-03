3aaec46f3076663665a8e1b32dd037fea6e9ff56
raubenravi <rubenrara@gmail.com>
2017-04-03 11:03:22 +0200

    rem.c

--- rem.c
+++ rem.c
@@ -5,7 +5,13 @@ if (afstand <= 7){
 			speed_right = 0;
 			motor[motorB] = speed_left;
 			motor[motorC] = speed_right;
+			break;
 }
+if (speed_left > speed_right){
+	speed_right = speed_left;
+} else {
+	speed_left = speed_right;
+	}
 clearTimer(T1);
 	while (speed_left > 0 && speed_right > 0){
 	//om de duizend microseconde word de snelheid bij gewerkt.

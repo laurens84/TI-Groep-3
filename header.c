cedf9b36a822265957fdfd7d60d125297f61da56
laurens84 <laurens.vandersluis@student.hu.nl>
2017-04-03 14:17:10 +0200

    Added comments and structure.

--- header.c
+++ header.c
@@ -1,19 +1,35 @@
-int speed_right = 0;
-int speed_left = 0;
-int btCmd = 0;
-int sound = 0;
+/// \mainpage Author Information
+/// \section Information
+///
+///     Name:          header.c
+///     Author:        Laurens van der Sluis
+///     Studentnumber: 1703647
+///     Date:          29-2-2017
+///
+/// \section doxygen How it works?
+///
+/// This file will be used by the entire team to define the global variables.
 
-//variables voor bluetooth.c
+//******************bluetooth.c***********************
 long nLastXmitTimeStamp = nPgmTime;
 long nDeltaTime = 0;
 const int kMaxSizeOfMessage = 30;
 const int INBOX = 5;
+int btCmd = 0;
 
+//******************collision.c**********************
 #define front 0
 #define left 1
 #define right 2
-int sensorstate = 0;
 
+int sensorstate = 0;
+int sound = 0;
+
+//*****************linefollower.c********************
+int speed_right = 0;
+int speed_left = 0;
+
+//*******************includes************************
 #include "bluetooth.c"
 #include "sounds.c"
 #include "linefollower.c"
--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/// \mainpage Author Information
+/// \section Information
+///
+///     Name:          main.c
+///     Author:        Laurens van der Sluis
+///     Studentnumber: 1703647
+///     Date:          29-2-2017
+///
+/// \section doxygen How it works?
+///
+/// This is the code where the hardware is defined and the robot will be started.
+
 #pragma config(Sensor, S1,     RGBLsensor,       sensorColorNxtRED)
 #pragma config(Sensor, S2,     RGBRsensor,      sensorColorNxtRED)
 #pragma config(Sensor, S3,     sonar,          sensorSONAR)
@@ -6,6 +18,17 @@
 #pragma platform(NXT)
 
 #include "header.c"
+
+/*! \brief main
+ *
+ * The robot will wait before the command to start is given.
+ * When the start command is given the robot will first check if there is an object in the way.
+ * The robot will start to follow the line as long as there is no object detected.
+ *
+ * If the command to stop is given, the robot will stop. If there was music playing, that will stop aswell.
+ * Now the robot again, will wait for the command to start.
+ *
+ */
 
 task main() {
   changeSpeedRampingParms(motorA, 15, 5, 15, 5);

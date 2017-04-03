2b823a40a521604e983a871693e10138e1b5ca50
GlennKoning <glenn_koning@icloud.com>
2017-04-03 12:33:27 +0200

    Updated for 2 RGB sensors

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#pragma config(Sensor, S1,     BWsensor,       sensorLightActive)
-#pragma config(Sensor, S2,     RGBsensor,      sensorColorNxtRED)
+#pragma config(Sensor, S1,     RGBLsensor,       sensorColorNxtRED)
+#pragma config(Sensor, S2,     RGBRsensor,      sensorColorNxtRED)
 #pragma config(Sensor, S3,     sonar,          sensorSONAR)
 #pragma config(Motor,  motorB,          rightMotor,    tmotorNXT, PIDControl, driveLeft, encoder)
 #pragma config(Motor,  motorC,          leftMotor,     tmotorNXT, PIDControl, driveRight, encoder)

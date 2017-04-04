78cd99a9b4133020265e9a0925f044c8e6899b43
raubenravi <rubenrara@gmail.com>
2017-04-04 11:34:27 +0200

    rem

--- rem.c
+++ rem.c
@@ -1,13 +1,15 @@
 //functie die remt
+
+//input int afstand, afstand tot object in centimeters 
 void rem(int afstand){
-if (afstand <= 10){
+if (afstand <= 7){
 	speed_left = 0;
 		speed_right = 0;
 		motor[motorB] = speed_left;
 		motor[motorC] = speed_right;
 		return;
 }
-
+//motors snelheid gelijk maken aan de langsaamste motor
 if (speed_left > speed_right){
 	speed_right = speed_left;
 } else {
@@ -15,7 +17,7 @@ if (speed_left > speed_right){
 	}
 clearTimer(T1);
 	while (speed_left > 0 && speed_right > 0){
-		
+		//nood stop als afstand kleiner is dan 7 cm
 		if (SensorValue[S3] <= 7){
 			speed_left = 0;
 			speed_right = 0;
@@ -26,9 +28,9 @@ clearTimer(T1);
 }
 		
 		//om de duizend microseconde word de snelheid bij gewerkt.
-	if (time1[T1] >= 350){
-			speed_left = speed_left -   ((30/ afstand) * 10 );
-			speed_right = speed_right - ((30/ afstand) * 10 );
+	if (time1[T1] >= 175){
+			speed_left = speed_left -   ((30/ afstand) * 5 );
+			speed_right = speed_right - ((30/ afstand) * 5 );
 
 			//checkt als niet 1 van snelheidswaarde niet op min staan
 			if (speed_left < 0){
